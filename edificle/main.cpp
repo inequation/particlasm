94d132c400875a28702b70db413548cac5ee7f94
Leszek Godlewski <lg@inequation.org>
2012-07-13 23:06:19 +0200

    initial wxWidgets-based editor work

---
+++ edificle/main.cpp
@@ -0,0 +1,14 @@
+/**
+edificle application source
+Copyright (C) 2012, Leszek Godlewski <lg@inequation.org>
+
+\author Leszek Godlewski
+*/
+
+#include "main.h"
+
+IMPLEMENT_APP(EdificleApp)
+
+bool EdificleApp::OnInit() {
+    return true;
+}
---
+++ edificle/main.h
@@ -0,0 +1,18 @@
+/**
+edificle application object header
+Copyright (C) 2012, Leszek Godlewski <lg@inequation.org>
+
+\author Leszek Godlewski
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <wx/wx.h>
+
+class EdificleApp : public wxApp {
+public:
+	virtual bool OnInit();
+};
+
+#endif // MAIN_H
